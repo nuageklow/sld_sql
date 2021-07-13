eb2d095cffd8fa5084abe74071e2894d708de2d8
nuageklow <nuageklow@gmail.com>
2021-07-13 09:12:48 +0800

    sql

---
+++ map_skill.sql
@@ -0,0 +1,8 @@
+/* skill lookup */
+CREATE TABLE lookup_table(
+skill text NOT NULL DEFAULT '',
+level text NOT NULL DEFAULT '',
+event_topic text NOT NULL DEFAULT '',
+chapter text NOT NULL DEFAULT '',
+event_type text NOT NULL DEFAULT '',
+
