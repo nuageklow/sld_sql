f9d346161cb9d26c671dacba97821b2ad115eae3
Karen W <38741470+nuageklow@users.noreply.github.com>
2022-02-11 23:15:55 +0800

    Update zoom_.py

--- zoom_.py
+++ zoom_.py
@@ -2,10 +2,17 @@ import csv, os
 import pandas as pd
 
 def remapping_cols(zoom_csv):
+    file_name = os.path.basename(zoom_csv)
     df = pd.read_csv(zoom_csv)
+    col_list = df.columns.tolist()
+    col_list = [col.replace(' ', '_').replace("'","").lower() for col in col_list]
+    df.columns = col_list
+
     print(df.columns)
+    # return df
 
+data_path = os.path.join(os.getcwd(), '..', 'data')
+csv_list = [os.path.join(data_path, csv_file) for csv_file in os.listdir(data_path) if csv_file[0].isdigit()]
 
-data_path = "D:\Documents\volunteer\SLD\data"
-csv_list = [csv_file for csv_file in os.listdir(data_path) if csv_file[0].isdigit()]
-print(csv_list)
+for each_csv in csv_list:
+    remapping_cols(each_csv)
