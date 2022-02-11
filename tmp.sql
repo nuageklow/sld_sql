4ff7b8f7fd2f094feff0ee1c7aae0a2387e3614c
karen <nuageklow@gmail.com>
2022-02-11 11:43:21 +0800

    sql

--- tmp.sql
+++ tmp.sql
@@ -1,24 +1,4 @@
--- zoom webinar - registration email cnts
-WITH
-pzwr AS (
-SELECT zwe.start_time, zwr.event_id, zwr.email
-FROM public.zoom_webinar_registration zwr
-INNER JOIN public.zoom_webinar_event zwe ON zwr.event_id = zwe.event_id
-GROUP BY zwr.event_id, zwe.start_time, zwr.email
-),
-tzwr AS (
-SELECT zwr.event_id, zwr.email
-FROM test.zoom_webinar_registration_v2 zwr
-GROUP by event_id, zwr.email
-)
-SELECT pzwr.start_time, pzwr.event_id AS p_event_id, pzwr.email, tzwr.event_id AS t_event_id, tzwr.email
-FROM pzwr
-FULL OUTER JOIN tzwr on pzwr.event_id = tzwr.event_id
-WHERE pzwr.event_id = tzwr.event_id
-GROUP BY pzwr.start_time, pzwr.event_id, tzwr.event_id, pzwr.email, tzwr.email
-ORDER BY pzwr.start_time ASC;
-
-
+-- write sql script to compare zoom webiste vs zoom api
 
 WITH email_list AS (
 SELECT pzwe.start_time, pzwr.event_id AS p_event_id, pzwr.email AS p_email, tzwr.event_id AS t_event_id, tzwr.email AS t_email
@@ -29,6 +9,6 @@ WHERE pzwr.event_id = tzwr.event_id AND tzwr.event_id IS NOT NULL
 ORDER BY pzwe.start_time ASC)
 SELECT *
 FROM email_list
-WHERE t_event_is IS NOT NULL AND t_email IS NOT NULL
+WHERE t_event_id IS NOT NULL AND t_email IS NOT NULL
 ORDER BY start_time ASC
 ;
