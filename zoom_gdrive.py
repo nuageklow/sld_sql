890c95b2096834b33b8b88771f3a35ba38037d67
karen <nuageklow@gmail.com>
2022-02-14 22:41:30 +0800

    sql

--- zoom_gdrive.py
+++ zoom_gdrive.py
@@ -1,17 +1,15 @@
-# import libs 
-import os, csv 
+import os, csv
 import pandas as pd
-import numpy as pd
+import numpy as np
 from google.colab import drive
 
 # connect gdrive
 drive.mount('/content/drive')
-p = os.path.join(os.getcwd(), 'drive', 'MyDrive', 'SLD Global Data', 'zoom_data')
-print(os.listdir(p))
-
+sld_path = os.path.join(os.getcwd(), 'drive', 'MyDrive', 'SLD Global Data', 'zoom_data')
+print(os.listdir(sld_path))
 
 def get_csvs(path, report_type):
-    ''' sort csv files downloaded from zoom '''
+  ''' sort csv files downloaded from zoom '''
   csv_list = []
   for i in os.listdir(path):
     # print(i.lower())
@@ -19,22 +17,55 @@ def get_csvs(path, report_type):
       csv_list.append(i)
   return csv_list
 
+def csv_cleaning(sld_path, csv_list):
+  for i in csv_list:
+    csv_p = os.path.join(sld_path, i)
+    with open(csv_p, 'r') as fin, open(f'{sld_path}/cleaned_{i}', 'w') as fout:
+      line_list = fin.readlines()
+      print(i, len(line_list))
+      # print(line_list)
+      for line in line_list:
+        if len(line) > 2:
+          if line[-2] == ',':
+            fout.write(line[:-2] + '\n')
+          else:
+            fout.write(line)
+        else:
+          fout.write(line)
+
+      # new_line_list = [line[:-1] for line in line_list if line[-1] == ',']
+      # fout.write(new_line_list)
+      fin.close()
+      fout.close()
 
 def csv_to_df(csv_file, sld_path):
   webinar_id = csv_file.split(' - ')[0]
-  print(f'processing {webinar_id}')
-  df = pd.read_csv(os.path.join(sld_path, csv_file))
+  print(f'processing {csv_file}')
+  csv_path = os.path.join(sld_path, csv_file)
+  # print(csv_path)
+  df = pd.read_csv(csv_path, skiprows=5)
+  # print(df.columns)
+  if 'First Name' not in df.columns.tolist():
+    df = pd.read_csv(csv_path)
+
   col_list = df.columns.tolist()
   col_list = [col.replace(' ', '_').replace("'","").lower() for col in col_list]
   df.columns = col_list
+  df['event_id'] = webinar_id
 
-  print(df.columns)
-  df.to_csv(f'remap_{file_name}')
-    # return df
 
-data_path = os.path.join(os.getcwd(), '..', 'data')
-csv_list = [os.path.join(data_path, csv_file) for csv_file in os.listdir(data_path) if csv_file[0].isdigit()]
+  # print(df.columns)
+  # df.to_csv(f'remap_{file_name}')
+  return df
 
+report_name = 'registration' # attend or reg
+csv_list = get_csvs(sld_path, report_name)
+# csv_list = get_csvs(drive_path)
+csv_cleaning(sld_path, csv_list)
+df = pd.DataFrame()
+df_list = list(map(lambda x: csv_to_df('cleaned_'+x,sld_path), csv_list))
+df = pd.concat(df_list, ignore_index=True)
+df.to_csv('{}/{}_cleaned.csv'.format(sld_path,report_name),index=False)
 
 def csv_to_df(i, sld_path):
   webinar_id = i.split(' - ')[0]
