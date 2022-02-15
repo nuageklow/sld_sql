3106bc2b5e6233b35177229e99806221d4d1bd91
Karen <nuageklow@gmail.com>
2022-02-15 08:53:43 +0800

    sql

--- zoom_fromweb.sql
+++ zoom_fromweb.sql
@@ -24,3 +24,6 @@ SELECT
 
 FROM
 )
+
+
+first_name	last_name	email	city	country/region	phone	organization	registration_time	approval_status	gender	whats_your_age_bracket?	in_which_industry_do_you_work?	if_selected_others_for_the_previous_question,_please_specify	what_is_the_functional_role_of_your_latest_job_position?	if_selected_others_for_previous_question,_please_specify	what_is_your_job_level?	how_did_you_find_out_about_this_event?	agree_to_terms_&_conditions_<https://shelovesdata.com/terms-and-conditions/>_and_privacy_policy_https://shelovesdata.com/privacy-policy/	country/region_name	attendance_type	event_id	what_is_your_job_level_?	agree_to_terms_&_conditions_<https://shelovesdata.com/terms-and-conditions/>_and_privacy_policy_<https://shelovesdata.com/privacy-policy/>	source_name	agree_to_terms_and_conditions_and_privacy_policy	attended_y/n	attended?	date_au	date_us	event_name	organizing_chapter	event_type	zoom_event_type	
