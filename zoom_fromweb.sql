9a13a850a5b3c432f2fa976f7fc4b5a8ae8b4224
karen <nuageklow@gmail.com>
2022-02-16 09:08:33 +0800

    sql

--- zoom_fromweb.sql
+++ zoom_fromweb.sql
@@ -1,3 +1,40 @@
+-- create schema
+CREATE SCHEMA zoom;
+
+CREATE TABLE zoom.clean_registration(
+  first_name text NULL,
+  last_name text NULL,
+  email text NULL,
+  city text NULL,
+  "country/region" text NULL,
+  phone int NULL,
+  organization text NULL,
+  registration_time text NULL,
+  approval_status text NULL,
+  gender text NULL,
+  "whats_your_age_bracket?" text NULL,
+  "in_which_industry_do_you_work?" text NULL,
+  "if_selected_others_for_the_previous_question,_please_specify" text NULL,
+  what_is_the_functional_role_of_your_latest_job_position? text NULL,
+  "if_selected_others_for_previous_question,_please_specify" text NULL,
+  what_is_your_job_level? text NULL,
+  how_did_you_find_out_about_this_event? text NULL,
+  "agree_to_terms_&_conditions_<https://shelovesdata.com/terms-and-conditions/>_and_privacy_policy_https://shelovesdata.com/privacy-policy/" text NULL,
+  "country/region_name" text NULL,
+  attendance_type text NULL,
+  event_id text NULL,
+  what_is_your_job_level_? text NULL,
+  "agree_to_terms_&_conditions_<https://shelovesdata.com/terms-and-conditions/>_and_privacy_policy_<https://shelovesdata.com/privacy-policy/>" text NULL,"source_name,agree_to_terms_and_conditions_and_privacy_policy" text NULL,
+  "attended_y/n" text NULL,
+  attended? text NULL,
+  date_au text NULL,
+  date_us text NULL,
+  event_name text NULL,
+  organizing_chapter text NULL,
+  event_type text NULL,
+  zoom_event_type text NULL
+);
+
 -- create table
 CREATE TABLE zoom.zoom_gui_registration (
   id text NOT NULL default ''::text,
@@ -23,6 +60,7 @@ CREATE TABLE zoom_gui_registration AS (
 SELECT
 
 FROM
+cleaned_registration
 )
 
 
