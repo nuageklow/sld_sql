170f7aaada22c44123dc5810687628707aa24bb9
karen <nuageklow@gmail.com>
2022-03-15 16:26:56 +0800

    sql

---
+++ fb.sql
@@ -0,0 +1,18 @@
+/* 
+
+post_clicks_unique
+post_engaged_users
+post_reactions_like_total
+post_impressions
+
+
+Command Line Interface Code
+FB_csv
+id	ex_account_id	fb_graph_node	parent_id	name	key1	key2	value	period	end_time	title	description
+
+*/
+
+
+WITH 
+p_clicks AS 
+SELECT * from xxx;
